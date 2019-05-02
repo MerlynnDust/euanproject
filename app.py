3a5ac02dc7b59b0f78cf37f72352136ecc2aed9b
amalthea <anthony.angel@x2i.systems>
2019-05-02 00:52:45 -0400

    initializing project. 3 methods and 3 wrapper functions so far specifying 3 seperate pages within Flask. No real database yet currently going to use a dictionary instead for now for json data. Also have an html page with 3 inputs

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import render_template
+app = Flask(__name__)
+
+database1 = {}
+
+@app.route('/ip')
+def front_page(name=None):
+    return render_template('front_page.html', name=name)
+
+@app.route('/ip', methods=["POST"])
+def hello_world():
+    ip_text = request.form['IP']
+    reason_text = request.form['Reason']
+    duration_text = request.form['Duration']
+
+    return render_template('form_submitted.html',ip_text=ip_text,reason_text=reason_text,duration_text=duration_text)
+
+@app.route("/check_ip", methods=["GET"])
+def get_my_ip():
+    if request.remote_addr in database1 == True:
+        return jsonify({'ip': request.remote_addr,'reason':})
+    else
+
+    return jsonify({'ip': request.remote_addr}), 200
+
+if __name__ == '__main__':
+    app.run()
+
+
+
